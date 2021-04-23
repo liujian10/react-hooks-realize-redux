0ee961b618414ed692a56278ef87739f0d349333
刘建 <liujian02@sunlands.com>
2021-04-23 16:49:15 +0800

    feat: use @maple-rc/redux-hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from '@maple-rc/redux-hook'
+import { Provider, combineReducers } from '@maple-rc/redux-hook'
 import App from './components/App.js'
 import Todos from './components/Todos.js'
 import Demo from './components/Demo.js'
@@ -8,7 +8,7 @@ import Demo from './components/Demo.js'
 import { reducers, initState } from './reducers/index'
 
 render(
-  <Provider reducers={reducers} initState={initState}>
+  <Provider reducer={combineReducers(reducers)} initState={initState}>
       <Todos />
       <App />
       <Demo />
